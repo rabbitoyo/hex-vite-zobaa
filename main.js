b2d420dbafc8ace932780fdc5955601e2a210b46
rabbit <night31916@gmail.com>
2025-08-24 21:45:58 +0800

    update login

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/effect-fade';
 import './assets/scss/all.scss';
+
+import Swiper from 'swiper';
+import { Pagination, Autoplay, EffectFade } from 'swiper/modules';
+Swiper.use([Pagination, Autoplay, EffectFade]);
 import 'bootstrap/dist/js/bootstrap.min.js';
 
-console.log('Hello world');
+document.addEventListener("DOMContentLoaded", function () {
+
+    const loginBanner = new Swiper('.loginBanner', {
+        effect: 'fade',
+    })
+
+});
